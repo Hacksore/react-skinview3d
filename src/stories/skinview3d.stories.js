6041330b287074071141755295924ae5f0891ba5
Sean Boult <hacksore@mcskinsearch.com>
2019-11-30 09:39:17 -0600

    Add storybook knobs

--- src/stories/skinview3d.stories.js
+++ src/stories/skinview3d.stories.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import Skinview3d from '..';
+import { radios, number } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs/react';
 
 export default {
-  title: 'Stories',
+  title: 'All stories',
+  decorators: [withKnobs]
 };
 
 export const basic = () => (
   <Skinview3d
     className='viewer'
-    skinUrl={require("../textures/skin-legacyhat-default-no_hd.png")}
+    skinUrl={require('../textures/skin-legacyhat-default-no_hd.png')}
     height="600"
     width="600"
   />
 );
+
+export const basicWithKnobs = () => {
+
+  const options = [
+    'textures/skin-legacyhat-default-no_hd.png',
+    'textures/skin-1.8-default-no_hd.png',
+    'textures/skin-1.8-slim-no_hd.png',
+    'textures/skin-old-default-no_hd.png',
+  ]
+
+  const value = radios('Skin URL', options, 'textures/skin-legacyhat-default-no_hd.png');
+
+  const numberOptions = {
+    range: true,
+    min: 128,
+    max: 1024,
+    step: 1,
+ };
+
+  const width = number('Width', 600, numberOptions);
+  const height = number('Height', 600, numberOptions);
+
+  return <Skinview3d
+    className='viewer'
+    skinUrl={require(`../${value}`)}
+    height={height}
+    width={width}
+  />
+};
 
 export const multiple = () => (
   <div style={{ display: 'flex', flexDirection: 'row' }}>
