2b528c75fc7cc77f9b4384e48209cbf6c9b799a5
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-10-03 20:38:34 +0000

    Update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-knobs",
-    "@storybook/preset-create-react-app"
+    "@storybook/addon-knobs"
   ]
 }
