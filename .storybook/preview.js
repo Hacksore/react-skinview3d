30a4df5efebed9a8b1df6ae1b0504461979c51a5
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-10-29 02:56:50 +0000

    Dark mode for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,14 @@
+import { themes } from "@storybook/theming";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  docs: {
+    theme: themes.dark,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
