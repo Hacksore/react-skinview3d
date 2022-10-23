f3f579ddb853835c829656bfa3be29fe59da0146
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-10-23 11:19:00 -0500

    Fix types

--- src/stories/skinview3d.stories.tsx
+++ src/stories/skinview3d.stories.tsx
@@ -4,6 +4,7 @@ import { withKnobs, radios, number } from "@storybook/addon-knobs";
 import * as skinview3d from "skinview3d";
 import { useRef, useState } from "react";
 import { SkinViewer } from "skinview3d";
+import { RadiosTypeOptionsProp } from "@storybook/addon-knobs/dist/components/types";
 
 export default {
   title: "All stories",
@@ -22,12 +23,15 @@ export const basic = () => (
 );
 
 export const basicWithKnobs = () => {
-  const options: any = [
-    "textures/skin-legacyhat-default-no_hd.png",
-    "textures/skin-1.8-default-no_hd.png",
-    "textures/skin-1.8-slim-no_hd.png",
-    "textures/skin-old-default-no_hd.png",
-  ];
+
+  // TODO: move to controls instead of knobs
+  // TIHI
+  const options: RadiosTypeOptionsProp<string> = {
+    "skin-legacyhat-default-no_hd.png": "textures/skin-legacyhat-default-no_hd.png",
+    "skin-1.8-default-no_hd.png": "textures/skin-1.8-default-no_hd.png",
+    "skin-1.8-slim-no_hd.png": "textures/skin-1.8-slim-no_hd.png",
+    "skin-old-default-no_hd.png": "textures/skin-old-default-no_hd.png",
+  }
 
   const value = radios("Skin URL", options, "textures/skin-legacyhat-default-no_hd.png");
 
