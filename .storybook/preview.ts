38434f1bd3d8d3dc99fcd96c9881871d02f58410
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-20 08:18:22 -0500

    Fix darkmode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,11 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
