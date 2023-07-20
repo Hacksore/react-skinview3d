fac9a879af9aee345d7f71500d295ce498207612
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-20 07:55:04 -0500

    Update deps and use workaround for SB

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-controls",
   ],
