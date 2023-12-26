132f62b3e8a929a3bfd80c8ef8c9b7868e8586e1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-26 08:25:27 -0600

    Switch to pnpm and update eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+const config = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+  },
+};
+
+module.exports = config;
